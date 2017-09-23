7edc85d069ffe6eb10c94dcfe3261f78959f9b22
Katie Lejman <misskatiemary@gmail.com>
2017-09-23 10:01:05 -0400

    WIP states

--- AJAXAPI.js
+++ AJAXAPI.js
@@ -4,9 +4,23 @@
 // SETTERS
 // Event Delegation
 
+var moviebtns = ["The Matrix", "Mel Brooks Movies", "Muppets", "Princess Bride", "You've Got Mail", "Animation", "Simpsons", "Toy Story", "John Hughes", "Harry Potter", "Disney", "Home Alone"];
+
+var movieData = {};
+
+
+var a = $("<button>");
+for(var i =0; i< moviebtns.length; i++) {
+    var a = $("<button>");
+    a.addClass("moviebtn");
+    a.attr('id', 'searchbtn');
+    $.data(a, 'movie', moviebtns[i]);
+    a.text(moviebtns[i]);
+    $('#movie-buttons').append(a);
+}
+
  $(document).on('click', "button", function () {
     var val = $(this).html(); // HTML values changing
-    //Comedy Movie Search- Button 1
     var endpoint= "http://api.giphy.com/v1/gifs/search?";
     var parameters= "q=" + val + "&limit=12&rating=PG&api_key=7faa75150cd64d5599def7be32bdf555";
     $.ajax({
@@ -15,7 +29,6 @@
     })
     //promise
         .then(function(response) {
-
             setImageArray(response);
         })
         .catch(function(err){
@@ -28,12 +41,20 @@ function setImageArray(response){
     var movies = $('#movies');
     movies.empty();
     for(var i = 0; i < images.length; i++){
+        var id = images[i].id;
         var src = images[i].images.fixed_width.url;
+        // var srcStill = images[i].images.fixed_width_still.url;
         var buttonImages = $('<img>');
+        $.data(buttonImages, "id", id);
+        movieData[id] = {};
+        movieData[id].still = images[i].images.fixed_width_still.url;
+        movieData[id].animated = images[i].images.fixed_width.url;
         buttonImages.attr('src', src);
         movies.append(buttonImages);
     }
 }
+
+
 $("#submit").on("click", function (event) {
     event.preventDefault();
 
@@ -41,7 +62,7 @@ $("#submit").on("click", function (event) {
     console.log(val);
     var button = $("<button>");
     button.html(val);
-    $(".movieButtons").append(button);
+    $("#movie-buttons").append(button);
     $("#movie-input").val("")
 });
 
